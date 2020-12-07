a8d4c8cdad38e4fbb220d55a20d220707f0644ad
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2020-12-07 22:01:17 +0100

    Better description for challenge 01

--- src/05-next-level/01/index.ts
+++ src/05-next-level/01/index.ts
@@ -7,7 +7,7 @@ type ZipCode = number | string | undefined | null;
   
   Checkout the type of arg "zipCode". As you noticed,
   neither null nor undefined is allowed. Therefore,
-  it accepts only number or string.
+  it accepts only number or string. Make the tests green.
 */
 export const formatZipCode = (zipCode: NonNullish<ZipCode>): string => {
   throw Error('Not implemented yet!')
